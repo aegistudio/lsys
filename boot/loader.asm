194b4707d36252af61f9d7e17004394e9c3c5d6c
root <root@ubuntu.ubuntu-domain>
2015-05-23 04:53:53 +0800

    Adding find directory function into the shared inclusion.

--- boot/loader.asm
+++ boot/loader.asm
@@ -10,17 +10,4 @@ loaderCode:
 	mov es, ax
 	mov ss, ax
 
-	mov ax, 1301h
-	mov bx, 0007h
-	mov cx, inLoaderNow.length
-	mov dl, 0
-	mov bp, inLoaderNow
-	int 10h
 	jmp $
-
-	resb 3000h
-
-	%include "boot/floppy.inc"
-
-	inLoaderNow db "You're now inside loader!!!"
-	inLoaderNow.length equ $ - inLoaderNow
