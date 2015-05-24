16c035e6eeaef884e761124d1348a7aee9671fa0
root <root@ubuntu.ubuntu-domain>
2015-05-25 02:31:11 +0800

    Now loader can load kernel.

--- boot/loader.asm
+++ boot/loader.asm
@@ -2,7 +2,7 @@
 ;			16 Bit Real Mode
 ; @Description: Now inside 16 bit real Mode Of Loader. Addressing uses
 ; 20 bit address line and segment registers. Now what we need to do is
-; loading kernel into 0050h:0000h, setup descriptors and page tables
+; loading kernel into 0100h:0000h, setup descriptors and page tables
 ; then translate to protected mode.
 ;********************************************************************
 bits 16
@@ -11,6 +11,7 @@ jmp loaderCode
 nop
 
 %include "boot/fat12header.inc"
+%include "protect.inc"
 
 loaderCode:
 	mov ax, cs
@@ -30,11 +31,34 @@ loaderCode:
 	mov bx, progress.length
 	call loader.writeToEnd
 
+	push es
+	mov ax, 0100h
+	mov es, ax
+
 	mov dx, kernel.elf
+	mov ax, rootEntry.areaSize + rootEntry.areaBeginSector
+	mov cx, rootEntry.areaBeginSector
+	mov bx, 0
+	call floppy.findDirectory
+	cmp ax, 0
+	jne kernel.findSuccess
+
+	pop es
+	mov ax, fail
+	mov bx, fail.length
+	call loader.writeToEnd
+	jmp $
+
+	kernel.findSuccess:
+	mov es, dx
+	pop es
 
 	mov ax, ok
 	mov bx, ok.length
 	call loader.writeToEnd
+
+	push es
+	mov es, dx
 
 	jmp $
 
