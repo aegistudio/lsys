cc126700197ca57b5df435b2d3643643f2cfb33d
aegistudio <1910567646@qq.com>
2015-06-18 19:38:44 +0800

    Adding system calls.

--- api/stdout_base.asm
+++ api/stdout_base.asm
@@ -1,7 +1,45 @@
 global asm_systemcall_stdout
+extern video_put_char
+extern video_put_string
+extern video_clear_screen
+extern video_set_cursor
 section .text:
+stdout_address_table:
+	dd	stdout_putchar
+	dd	stdout_putstring
+	dd	stdout_clear
+	dd	stdout_cursor
 
 asm_systemcall_stdout:
-	jmp $
+	cli
+	push eax
+	push cs
+	push dword[cs : stdout_address_table + (eax * 4)]
 	retf
+
+	stdout_putchar:
+		push esi
+		push edi
+		call video_put_char
+		add esp, 8
+		jmp stdout_endofcall
+	stdout_putstring:
+		push esi
+		push edi
+		call video_put_string
+		add esp, 8
+		jmp stdout_endofcall
+	stdout_clear:
+		call video_clear_screen
+		jmp stdout_endofcall
+	stdout_cursor:
+		push esi
+		push edi
+		call video_set_cursor
+		add esp, 8
+		jmp stdout_endofcall
+	stdout_endofcall:
+	pop eax
+	sti
+	iretd
 
--- include/systemcall.h
+++ include/systemcall.h
@@ -7,11 +7,15 @@
 	#define __systemcall_api	extern
 #endif
 
+/** Trap Certain System Call Number To Get To A System Call. ***/
+/** Pass The Sub Functin Number To The System Call By EAX Register ***/
+/** Pass Other Parameters In The Order Of EDI, ESI, EDX, ECX, EBX **/
+
 #define systemcall_stdout	0x21
-#define stdout_putchar		0x00
-#define stdout_putstring	0x01
-#define stdout_clear		0x02
-#define stdout_cursor		0x03
+#define stdout_putchar		0x00	/** EDI = CHAR TO PUT, ESI = COLOR OF THE CHAR **/
+#define stdout_putstring	0x01	/** EDI = BEGIN OF STRING ADDRESS, ESI = COLOR OF THE STRING **/
+#define stdout_clear		0x02	/** NO PARAM IS NEEDED	**/
+#define stdout_cursor		0x03	/** EDI = THE ROW OF THE CURSOR, ESI = THE COLUMN OF THE CURSOR **/
 
 __systemcall_api void systemcall_stdout_initialize();
 
@@ -21,5 +25,7 @@ __systemcall_api void systemcall_stdout_initialize();
 #define process_yield		0x00
 #define process_sleep		0x01
 #define process_terminate	0x02
+
+#define systemcall_semaphore	0x24
 
 #endif
